7330919fc46da274ef25c4fdf1cb407bb6d6f011
wenxian.zhang <wenxian.zhang@servicenow.com>
2024-10-15 13:21:08 -0700

    set verify to False for nowllm

--- utils.py
+++ utils.py
@@ -394,7 +394,7 @@ def now_tgi(model, messages, temperature, max_tokens, api_dict=None):
         })
         # print("payload: ", payload)
         # print("before inference:", os.system("df -h /tmp"))
-        response = requests.post(url, headers=headers, data=payload)
+        response = requests.post(url, headers=headers, data=payload, verify=False)
         print("response: ", response)
         output = response.json()["generated_text"]
         # print("output: ", output)
